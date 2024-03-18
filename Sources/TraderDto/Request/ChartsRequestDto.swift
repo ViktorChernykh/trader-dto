521e7fd103e6acfbfaf47ca77164193aa50088dd
ViktorChernykh <v-cher9@yandex.ru>
2024-03-18 19:42:40 +0300

    Add ChartRequestDto

--- Sources/TraderDto/Request/ChartsRequestDto.swift
+++ Sources/TraderDto/Request/ChartsRequestDto.swift
@@ -1,5 +1,5 @@
 //
-//  ChartRequestDto.swift
+//  ChartsRequestDto.swift
 //
 //
 //  Created by Victor Chernykh on 18/03/2024.
@@ -7,6 +7,45 @@
 
 import Foundation
 
+public struct ChartsRequestDto: Codable {
+	// MARK: - Stored properties
+	public let charts: [ChartRequestDto]
+
+	// MARK: - Init
+	public init(charts: [ChartRequestDto]) {
+		self.charts = charts
+	}
+}
+
 public struct ChartRequestDto: Codable {
-	public let charts: []
+	// MARK: - Stored properties
+	public let engine: String
+	public let market: String
+	public let board: String
+	public let security: String
+	public let interval: Int
+	public let from: Date?
+	public let till: Date
+	public let limit: Int?
+
+	// MARK: - Init
+	public init(
+		engine: String,
+		market: String,
+		board: String,
+		security: String,
+		interval: Int,
+		from: Date?,
+		till: Date,
+		limit: Int?
+	) {
+		self.engine = engine
+		self.market = market
+		self.board = board
+		self.security = security
+		self.interval = interval
+		self.from = from
+		self.till = till
+		self.limit = limit
+	}
 }
